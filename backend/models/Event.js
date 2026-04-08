a453b386f045c4ef777d8553839ee48c5029db83
olayemi omolisa <olayemiomolisa@yahoo.com>
2026-04-08 17:52:46 +0100

    backend fix

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,10 +1,38 @@
 // models/Event.js
 import mongoose from "mongoose";
 
-const eventSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    image: String,
-}, { timestamps: true });
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    image: {
+      type: String, // thumbnail
+    },
+
+    type: {
+      type: String,
+      enum: ["video", "audio"], 
+      default: "video",
+    },
+
+    src: {
+      type: String, 
+      required: true,
+    },
+
+    category: {
+      type: String,
+      enum: ["Worship", "Teachings", "Mentorship", "Prayer", "Sermons"],
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Event", eventSchema);
