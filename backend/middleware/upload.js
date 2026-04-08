36b37c1b17d43d18b4772522c86062b67c09e462
olayemi omolisa <olayemiomolisa@yahoo.com>
2026-04-08 14:53:20 +0100

    upload

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,11 +1,13 @@
-// middleware/upload.js
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({
-    destination: "uploads/",
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "fameuntold",
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
 });
 
 const upload = multer({ storage });
